3e12cc0fce96c85ecf9228a7bcb767615e666285
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2021-07-01 23:22:29 -0300

    Criação de menu e metodos

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -1,7 +1,53 @@
-namespace DIO.Bank.Conta
+using System;
+
+
+namespace DIO.Bank 
 {
     public class Conta
     {
-        
+        private TipoConta TipoConta { get; set; }          
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }       
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+    {
+        this.TipoConta = tipoConta;
+        this.Saldo = saldo;
+        this.Credito = credito;
+        this.Nome = nome;
     }
+    public bool Sacar(double valorSaque)
+    {
+        if(this.Saldo - valorSaque < (this.Credito *-1)){
+            Console.WriteLine("Saldo insuficiente");
+            return false;
+        }
+        this.Saldo -= valorSaque;
+        Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        return true;
+    }
+    public void Depositar(double valorDeposito)
+    {
+        this.Saldo += valorDeposito;
+        Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+    }
+    public void Transferir(double valorTransferencia, Conta contaDestino)
+    {
+        if(this.Sacar(valorTransferencia))
+        {
+            contaDestino.Depositar(valorTransferencia);
+        }
+    }
+    public override string ToString()
+    {
+        string retorno = "";
+        retorno += "TipoCampo " + this.TipoConta + " | ";
+        retorno += "Nome " + this.Nome + " | ";
+        retorno += "Saldo " + this.Saldo + " | ";
+        retorno += "Credito " + this.Credito;
+        return retorno;
+    }
+    
+    }    
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,81 @@ namespace DIO.Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+             while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        TransferirConta();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                        case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+                opcaoUsuario =  ObterOpcaoUsuario();
+            }
+            System.Console.WriteLine("Obrigado por usar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ListarContas()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InserirConta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void TransferirConta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Sacar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Depositar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine( );
+            Console.WriteLine("DIO Bank a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1- Listar Contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("x- Sair");
+            Console.WriteLine();
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
